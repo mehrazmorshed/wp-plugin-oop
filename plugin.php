9ec62e873088dd8c910abf45df5826964c3a7eff
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-11-28 11:52:35 +0600

    plugin file header

--- plugin.php
+++ plugin.php
@@ -1,1 +1,19 @@
 <?php
+/*
+ * Plugin Name:       Plugin Name
+ * Plugin URI:        https://wordpress.org/plugins/plugin-slug/
+ * Description:       Plugin short description.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mehraz Morshed
+ * Author URI:        https://profiles.wordpress.org/mehrazmorshed/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       plugin-text-domain
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
